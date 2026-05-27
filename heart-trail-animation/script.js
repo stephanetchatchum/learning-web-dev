027d2107680ca568090450294a8ee8e8ea61237d
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-05-27 13:00:21 +0200

    polishing the motion

--- heart-trail-animation/script.js
+++ heart-trail-animation/script.js
@@ -5,5 +5,12 @@ bodyEl.addEventListener("mousemove", ()=>{
     const spanEl = document.createElement("span");
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
+    const size =  Math.random()*100;
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
     bodyEl.appendChild(spanEl);
-})
+    setTimeout(()=>{
+        spanEl.remove();
+    }, 3000);
+});
+
