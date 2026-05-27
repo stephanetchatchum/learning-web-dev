3ff48afccbf04ad20f5cb958f179bfc55c0f84b4
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-05-27 12:49:12 +0200

    spamming hearts

---
+++ heart-trail-animation/script.js
@@ -0,0 +1,9 @@
+const bodyEl = document.querySelector("body")
+bodyEl.addEventListener("mousemove", ()=>{
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    bodyEl.appendChild(spanEl);
+})
