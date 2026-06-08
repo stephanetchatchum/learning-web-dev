2e99af0e0ac991422a6bc3bc22b4b539a391f8a5
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-08 12:28:59 +0200

    continuation

--- Personal-financial-tracker/script.js
+++ Personal-financial-tracker/script.js
@@ -1,6 +1,7 @@
 const addTransForm = document.getElementById("add-transaction-form");
 const addTransButton = document.getElementById("add-transaction");
 const cancelTransaction = document.getElementById("cancel");
+const transForm = document.getElementById("transaction-form");
 
 addTransButton.addEventListener('click', () => {
     addTransForm.style.display = 'block';
@@ -10,4 +11,17 @@ addTransButton.addEventListener('click', () => {
 cancelTransaction.addEventListener('click', () => {
     addTransForm.style.display = 'none';
     console.log("Closed form")
+});
+
+transForm.addEventListener("submit", function(submit) {
+    submit.preventDefault();
+
+    const formData = new FormData(transForm);
+
+    const formProps = Object.fromEntries(formData);
+
+    localStorage.setItem('savedFormData', JSON.stringify(formProps))
+    console.log('Save to local storage:', formProps)
+
+    alert('Form data saved successfully!')
 });
